57532a396d42af39c6dab32f0605c97263eba579
Matt Frisbie <msfrisbie@gmail.com>
2024-11-11 20:33:18 -0600

    dropdown select

--- src/App.js
+++ src/App.js
@@ -3,7 +3,38 @@ import "./App.css";
 import flus from "./flus.png";
 
 function App() {
-  // State to hold the elapsed time in various units
+  const chicagoBearsEvents = [
+    {
+      eventDescriptor: "won a road game on Sunday.",
+      dateTime: "2021-12-26T00:00:00Z",
+    },
+    {
+      eventDescriptor: "beat the Packers",
+      dateTime: "2020-12-29T00:00:00Z",
+    },
+    {
+      eventDescriptor: "made the playoffs",
+      dateTime: "2020-01-05T00:00:00Z",
+    },
+    {
+      eventDescriptor: "won a playoff game",
+      dateTime: "2011-01-16T00:00:00Z",
+    },
+    {
+      eventDescriptor: "won the division",
+      dateTime: "2018-12-16T00:00:00Z",
+    },
+    {
+      eventDescriptor: "made the Superbowl",
+      dateTime: "2007-01-21T00:00:00Z",
+    },
+    {
+      eventDescriptor: "won the Superbowl",
+      dateTime: "1986-01-26T00:00:00Z",
+    },
+  ];
+
+  const [selectedEvent, setSelectedEvent] = useState(chicagoBearsEvents[0]);
   const [timeElapsed, setTimeElapsed] = useState({
     years: 0,
     months: 0,
@@ -15,10 +46,8 @@ function App() {
   });
 
   useEffect(() => {
-    // Get the start date in milliseconds (January 1, 2010)
-    const startDate = new Date("2021-12-26T00:00:00Z").getTime();
+    const startDate = new Date(selectedEvent.dateTime).getTime();
 
-    // Function to update the elapsed time in different units
     const updateElapsedTime = () => {
       const currentTime = Date.now();
       let milliseconds = currentTime - startDate;
@@ -52,12 +81,9 @@ function App() {
       });
     };
 
-    // Set an interval to update the elapsed time every millisecond
     const intervalId = setInterval(updateElapsedTime, 1);
-
-    // Clear the interval on component unmount
     return () => clearInterval(intervalId);
-  }, []);
+  }, [selectedEvent]);
 
   const handleShare = async () => {
     if (navigator.share) {
@@ -77,56 +103,77 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={flus} className="App-logo" alt="logo" />
-        <h1>
-          <div className="huge">NO</div>
-          <div>Matt Eberflus has not been fired yet.</div>
-        </h1>
-        <h2>Things he's busy with instead of winning football games:</h2>
-        <div>Refining the HITS principle</div>
-        <div>Looking inward</div>
-        <div>Developing complementary football strategies</div>
-        <div>Searching for the "why's"</div>
-        <div>Focusing on the game plan</div>
-        <div>Going back to look at the tape</div>
-        <div>Seeing how it goes</div>
-        <div>Executing and preparing</div>
-        <div>Working on fundamentals and process</div>
-        <div>Evaluating everything and all phases</div>
-        <div>Staying the course</div>
-        <div>Taking it one day at a time</div>
-        <div>Visiting the barber 3x/week</div>
-        <div>Keeping the team on track</div>
-        <div>Reinforcing discipline and focus</div>
-        <div>Assessing player performance</div>
-        <div>Reviewing and refining strategies</div>
-        <div className="timer">
+    <>
+      <div className="timer">
+        <span>
           It's been&nbsp;
           <span className="fixed-time">{timeElapsed.years}</span> years,{" "}
           <span className="fixed-time">{timeElapsed.months}</span> months,{" "}
-          <span className="fixed-time">{timeElapsed.days}</span> days,&nbsp;
+          <span className="fixed-time">{timeElapsed.days}</span> days,{" "}
           <span className="fixed-time">{timeElapsed.hours}</span> hours,{" "}
           <span className="fixed-time">{timeElapsed.minutes}</span> minutes,{" "}
           <span className="fixed-time">
             {timeElapsed.seconds.toString().padStart(2, "0")}.
             {timeElapsed.milliseconds.toString().padStart(3, "0")}
           </span>{" "}
-          seconds since the Bears won a road game on Sunday.
-        </div>
+          seconds since the Bears&nbsp;
+          <select
+            value={selectedEvent.eventDescriptor}
+            onChange={(e) =>
+              setSelectedEvent(
+                chicagoBearsEvents.find(
+                  (event) => event.eventDescriptor === e.target.value
+                )
+              )
+            }
+            className="select"
+          >
+            {chicagoBearsEvents.map((event) => (
+              <option key={event.dateTime} value={event.eventDescriptor}>
+                {event.eventDescriptor}
+              </option>
+            ))}
+          </select>
+        </span>
+      </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={flus} className="App-logo" alt="logo" />
+          <h1>
+            <div className="huge">NO</div>
+            <div>Matt Eberflus has not been fired yet.</div>
+          </h1>
+          <h2>Things he's busy with instead of winning football games:</h2>
+          <div>Refining the HITS principle</div>
+          <div>Looking inward</div>
+          <div>Developing complementary football strategies</div>
+          <div>Searching for the "why's"</div>
+          <div>Focusing on the game plan</div>
+          <div>Going back to look at the tape</div>
+          <div>Seeing how it goes</div>
+          <div>Executing and preparing</div>
+          <div>Working on fundamentals and process</div>
+          <div>Evaluating everything and all phases</div>
+          <div>Staying the course</div>
+          <div>Taking it one day at a time</div>
+          <div>Visiting the barber 3x/week</div>
+          <div>Keeping the team on track</div>
+          <div>Reinforcing discipline and focus</div>
+          <div>Assessing player performance</div>
+          <div>Reviewing and refining strategies</div>
 
-        <div>
-          <button className="share" onClick={handleShare}>
-            #savecaleb
-          </button>
-        </div>
+          <div>
+            <button className="share" onClick={handleShare}>
+              #savecaleb
+            </button>
+          </div>
 
-        <a className="footer" href="https://mattfriz.com">
-          mattfriz.com
-        </a>
-      </header>
-    </div>
+          <a className="footer" href="https://mattfriz.com">
+            mattfriz.com
+          </a>
+        </header>
+      </div>
+    </>
   );
 }
 
